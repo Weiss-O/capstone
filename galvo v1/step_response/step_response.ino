e22eaca8f0c148d7c245ad1be4a361a894f1fde3
spoea <aspoelst@uwaterloo.ca>
2024-10-07 14:51:04 -0400

    arduino control code that works with galvo v1

--- galvo v1/step_response/step_response.ino
+++ galvo v1/step_response/step_response.ino
@@ -1,13 +1,15 @@
-// testestest
 // add after measurement. This is the range of the photodiodes
-const int topMax = 708;
-const int bottomMax = 693;
-const int topMin = 60;
-const int bottomMin = 54;
+const int16_t topMax = 708;
+const int16_t bottomMax = 693;
+const int16_t topMin = 60;
+const int16_t bottomMin = 54;
+
+// corresponds to 0.9V motor minimum at 5V supply
+const int16_t minPWM = 46;
 
 // controller gains
-const float kp = 0.15;
-const int ki = 0;
+const float kp = 10;
+const float ki = 0;
 
 // max angular range from -theta to +theta
 const float angleRange = 22.5;
@@ -19,9 +21,16 @@ const float angleOffset = (angleRange/2) - slope*(topMax - bottomMin);
 float integralErrorTotal = 0;
 unsigned long previousTime = 0;
 
-const int directionPin1 = 7;
-const int directionPin2 = 8;
-const int speedPin = 6;
+const uint8_t directionPin1 = 7;
+const uint8_t directionPin2 = 8;
+const uint8_t speedPin = 6;
+
+// Lookup table parameters
+const uint16_t tableSize = 360;  // Number of entries in the sine lookup table
+float sineLookup[tableSize];  // Sine wave lookup table
+float A = 10;  // Amplitude (Degrees)
+float f = 0.2; // Frequency (Hz)
+unsigned long period_us;  // Period in microseconds
 
 void setup() {
   // Initialize the serial communication at a higher baud rate
@@ -34,25 +43,32 @@ void setup() {
   pinMode(directionPin1, OUTPUT);
   pinMode(directionPin2, OUTPUT);
   pinMode(speedPin, OUTPUT);
+
+  // Precompute the sine lookup table
+  for (int i = 0; i < tableSize; i++) {
+      float angle = (2.0 * PI * i) / tableSize;  // Angle in radians
+      sineLookup[i] = A * sin(angle);  // Scaled by amplitude
+  }
+
+  // Calculate the period in microseconds based on frequency
+  period_us = 1000000 / f;  // 1 second (1e6 us) divided by frequency
  }
 
 void loop() {
   // Record the current time in microseconds
   unsigned long currentTime = micros();
   
-  // generate reference signal
-  float A = 2; // reference angle amplitude (Degrees)
-  float f = 0.1; // reference angle frequency (Hz)
+  // Find the corresponding index in the lookup table
+  unsigned long timeInCycle = currentTime % period_us;  // Time within one period
+  int tableIndex = map(timeInCycle, 0, period_us, 0, tableSize - 1);  // Map time to table index
 
-  // for sine wave input
-  float refAngle = A * sin((f *2.0 * PI * float(currentTime)) / float(1000000));
-  
-  // for a step input. Comment this out if you want an actual sine wave
-  /*if(refAngle > 0){
-    refAngle = A;
+  // Get the reference angle from the lookup table
+  float refAngle = sineLookup[tableIndex];
+  /*if (refAngle < 0){
+    refAngle = -5;
   }
-  else{
-    refAngle = 0;
+  else {
+    refAngle = 5;
   }*/
 
   // Read analog values from A0 and A1
@@ -67,43 +83,47 @@ void loop() {
   float error = refAngle - mirrorAngle;
 
   // add the error to the total error counter
-  integralErrorTotal += (error * (currentTime - previousTime) * (1/1000000));
+  //integralErrorTotal += (error * (currentTime - previousTime))*0.000001;
 
   // generate control signal using error, total error and gains
-  float commandSignal = (kp * error) + ki * (integralErrorTotal);
-
-  if(commandSignal < -255){
-    commandSignal = -255;
-  }
-  else if(commandSignal > 255){
-    commandSignal = 255;
-  }
+  int16_t commandSignal = (kp * error) + ki * integralErrorTotal;
 
   // send commands to the l298n
-  if(commandSignal < 0){
+  if(commandSignal < -10){
     digitalWrite(directionPin1, LOW);
     digitalWrite(directionPin2, HIGH);
+    commandSignal -= minPWM;
   }
-  else{
+  else if (commandSignal > 10){
     digitalWrite(directionPin1, HIGH);
     digitalWrite(directionPin2, LOW);
+    commandSignal += minPWM;
   }
-  digitalWrite(speedPin, abs(commandSignal));
+  else {
+    digitalWrite(directionPin1, LOW);
+    digitalWrite(directionPin2, LOW);
+  }
+  
+  commandSignal = constrain(commandSignal, -255, 255);
+  if (abs(commandSignal) < 10){
+    commandSignal = 0;
+  }
+  analogWrite(speedPin, abs(commandSignal));
 
   //prints in the following format
   // Current time [us], Angle (measured), Angle (reference), Error, Total integral error, Command Signal
-  Serial.print(currentTime);
-  Serial.print(",");
-  Serial.print(mirrorAngle);
-  Serial.print(",");
-  Serial.print(refAngle);
-  Serial.print(",");
-  Serial.print(error);
-  Serial.print(",");
-  Serial.print(integralErrorTotal);
-  Serial.print(",");
-  Serial.println(commandSignal);
-  
+  if (currentTime % 10000 < 500) {
+      //Serial.print(currentTime);
+      //Serial.print(",");
+      Serial.print(mirrorAngle);
+      Serial.print(",");
+      Serial.println(refAngle);
+      //Serial.print(",");
+      //Serial.println(integralErrorTotal);
+      //Serial.print(error);
+      //Serial.print(",");
+      //Serial.println(commandSignal);
+  }
   previousTime = currentTime;
   
   }
