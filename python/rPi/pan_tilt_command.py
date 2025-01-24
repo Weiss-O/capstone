96d0195bb9f4b63e0c54ae11701c4ed915756869
Owen Weiss <owasweiss@gmail.com>
2025-01-24 18:06:19 -0500

    Testing commanded pan and tilt coordinates

---
+++ python/rPi/pan_tilt_command.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import serial # Import the serial library
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.reset_input_buffer()
+
+    while True:
+        #Wait for me to type a command into the command line and then send that command to the Arduino
+        command = input("Type positioning command->") + "\n"
+        ser.write(command.encode())
+        #Wait for the Arduino to respond
+        while ser.in_waiting == 0:
+            pass
+        #Read the response from the Arduino
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+
