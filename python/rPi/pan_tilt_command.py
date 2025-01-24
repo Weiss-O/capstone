c43216bdb9794e2935f38aee54f8cefd273dcab5
Owen Weiss <owasweiss@gmail.com>
2025-01-24 18:10:42 -0500

    Added a timeout so the raspberry pi will not wait forever for a response

--- python/rPi/pan_tilt_command.py
+++ python/rPi/pan_tilt_command.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import serial # Import the serial library
+import time
 
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
@@ -9,9 +10,13 @@ if __name__ == '__main__':
         #Wait for me to type a command into the command line and then send that command to the Arduino
         command = input("Type positioning command->") + "\n"
         ser.write(command.encode())
-        #Wait for the Arduino to respond
+        #Wait for the Arduino to respond with timeout
+        start_time = time.time()
+        timeout = 10  # 5 second timeout
         while ser.in_waiting == 0:
-            pass
+            if time.time() - start_time > timeout:
+                print("Error: Timeout waiting for Arduino response")
+                break
         #Read the response from the Arduino
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
