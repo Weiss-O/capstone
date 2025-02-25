dc45dd1325ebea0d3bf7ade47b5407825adc6b65
Owen Weiss <owasweiss@gmail.com>
2025-02-25 14:58:10 -0500

    Fixed xircular import in OPO script.

--- python/OPO.py
+++ python/OPO.py
@@ -1,12 +1,11 @@
 from abc import ABC, abstractmethod
 import yaml
-import Detector
 
 with open('config.yaml') as file:
     config = yaml.safe_load(file)
 
 class Object():
-    def __init__(self, detection:Detector.Detection, POSID):
+    def __init__(self, detection, POSID):
         self.camera_position = config["baseline"][POSID]["camera_pos"] 
         self.bbox = detection.get_as_array() #Bounding box [x, y, w, h]
         self.center_coordinate = [self.bbox[0] + self.bbox[2]/2, self.bbox[1] + self.bbox[3]/2, 1] #Center coordinate [x, y, 1]
