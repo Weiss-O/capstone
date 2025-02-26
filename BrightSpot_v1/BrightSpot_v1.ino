9524736f32ae9d62fa673063f4db4ca91dd0e3ac
spoea <aspoelst@uwaterloo.ca>
2025-02-26 17:30:15 -0500

    Added master teensy files along with config, stepper and projection files

---
+++ BrightSpot_v1/BrightSpot_v1.ino
@@ -0,0 +1,17 @@
+#include "config.h"
+#include "project.h"
+// #include stepper_point.h
+// #include listen.h
+// #include fan.h
+// #include home.h
+
+void setup() {
+  // put your setup code here, to run once:
+  init_project();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
