e98ce8173c11caf0ff40cd2d9989cad5c978eccc
spoea <aspoelst@uwaterloo.ca>
2025-03-07 15:29:23 -0500

    Added pid controls for circle control and zeroing. Didn't remove the old function and registers in case we need it later

--- BrightSpot_v1/project.h
+++ BrightSpot_v1/project.h
@@ -9,7 +9,11 @@ struct MirrorAngles {
 };
 
 float circle_control(float u_new, bool is_x);
+float circle_control_PID(float u_new, bool is_x);
+
 bool center_mirrors(float threshold_error);
+bool center_mirrors_PID(float threshold_error);
+
 MirrorAngles get_mirror_angles();
 float command_motors(int motor_pin1, int motor_pin2, float u);
 bool project_circle(int duration, float magnitude, float frequency);
