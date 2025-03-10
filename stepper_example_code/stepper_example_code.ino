be58bc762ab0a41e8490e90d55ea1bc662e86cb8
Owen Weiss <owasweiss@gmail.com>
2025-03-10 19:11:25 -0400

    Added stepper example code to check whether or not the torque issue is a result of the stepper library being used.

---
+++ stepper_example_code/stepper_example_code.ino
@@ -0,0 +1,47 @@
+
+/*
+ Stepper Motor Control - one revolution
+
+ This program drives a unipolar or bipolar stepper motor.
+ The motor is attached to digital pins 8 - 11 of the Arduino.
+
+ The motor should revolve one revolution in one direction, then
+ one revolution in the other direction.
+
+
+ Created 11 Mar. 2007
+ Modified 30 Nov. 2009
+ by Tom Igoe
+
+ */
+
+#include <Stepper.h>
+#define STEPPER_PAN_1 6
+#define STEPPER_PAN_2 7
+#define STEPPER_PAN_3 8
+#define STEPPER_PAN_4 9
+const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+// for your motor
+
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, STEPPER_PAN_1, STEPPER_PAN_2, STEPPER_PAN_3, STEPPER_PAN_4);
+
+void setup() {
+  // set the speed at 60 rpm:
+  myStepper.setSpeed(60);
+  // initialize the serial port:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // step one revolution  in one direction:
+  Serial.println("clockwise");
+  myStepper.step(stepsPerRevolution);
+  delay(500);
+
+  // step one revolution in the other direction:
+  Serial.println("counterclockwise");
+  myStepper.step(-stepsPerRevolution);
+  delay(500);
+}
+
