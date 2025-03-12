72bbefa201aaed5a89fd6c93c5751df1d44d6aae
Owen Weiss <owasweiss@gmail.com>
2025-03-12 01:48:10 -0400

    tuning

--- BrightSpot_v1/stepper_point.cpp
+++ BrightSpot_v1/stepper_point.cpp
@@ -4,7 +4,7 @@
 #include <AccelStepper.h>
 
 #define STEPPER_SPEED 250
-#define STEPPER_ACCEL 50
+#define STEPPER_ACCEL 1000
 
 const int stepsPerRevolution = 2048; // Steps for full rotation
 const int positionLimit = -55; // corresponds to 10 degrees past the limit switch (10/360)*2048
