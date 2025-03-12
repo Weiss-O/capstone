61defc3f4ec1f019b67b29b3c675cfdc95649080
Owen Weiss <owasweiss@gmail.com>
2025-03-12 14:58:38 -0400

    tuning 5

--- BrightSpot_v1/project.cpp
+++ BrightSpot_v1/project.cpp
@@ -11,8 +11,8 @@ const float kp_y = 600.0;
 const float ki_y = 0.1;
 const float kd_y = 150;
 
-const float kp_c = 60;
-const float ki_c = 0.3;
+const float kp_c = 65;
+const float ki_c = 0.5;
 const float kd_c = 1;
 
 float sumErr_x = 0.0;
