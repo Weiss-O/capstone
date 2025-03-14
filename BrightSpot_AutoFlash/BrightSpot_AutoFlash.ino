7d31df47215ec585f89d6e82dec1947fab131f2f
Owen Weiss <owasweiss@gmail.com>
2025-03-14 03:52:03 -0400

    Working autoflash

---
+++ BrightSpot_AutoFlash/BrightSpot_AutoFlash.ino
@@ -0,0 +1,25 @@
+#include "config.h"
+#include "communication.h"
+#include "project.h"
+#include "stepper_point.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  init_comms();
+  init_project();
+  init_stepper();
+
+  // setup the fan pin
+  pinMode(FAN_PIN, OUTPUT);
+
+  //turn on the fan here
+  digitalWrite(FAN_PIN, HIGH);
+
+}
+
+void loop() {
+  // repeatedly check for something to read from the Pi
+  while (!Serial.available()) {}
+  String command = Serial.readStringUntil('\n'); // Read command
+  pi_communications(command);
+}
