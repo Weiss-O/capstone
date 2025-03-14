9cb4305efe6d2b0ed25985cab184eab71b1fa166
Owen Weiss <owasweiss@gmail.com>
2025-03-14 02:11:41 -0400

    Testing autoflash

--- BrightSpot_v1/communication.cpp
+++ BrightSpot_v1/communication.cpp
@@ -41,7 +41,7 @@ void pi_communications(String command) {
   }
 
   else if (commandChar == 'Z'){
-    disableMotors(); //Adding comment to introduce a change for testing autoflash
+    disableMotors(); //Adding comment to introduce a change for testing autoflash 1
     Serial.println("S");
   }
 
