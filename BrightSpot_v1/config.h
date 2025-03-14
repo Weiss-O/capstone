c55c43d2549fb594666637089e03314bca893f56
Owen Weiss <owasweiss@gmail.com>
2025-03-14 03:30:02 -0400

    Testing autoflash

--- BrightSpot_v1/config.h
+++ BrightSpot_v1/config.h
@@ -18,15 +18,15 @@
 #define SWITCH_PAN 19
 
 //stepper motor pins
-#define STEPPER_TILT_1 2
-#define STEPPER_TILT_2 3
-#define STEPPER_TILT_3 4
-#define STEPPER_TILT_4 5
-
 #define STEPPER_PAN_1 6
 #define STEPPER_PAN_2 7
 #define STEPPER_PAN_3 8
 #define STEPPER_PAN_4 9
+
+#define STEPPER_TILT_1 2
+#define STEPPER_TILT_2 3
+#define STEPPER_TILT_3 4
+#define STEPPER_TILT_4 5
 
 //trigger pins
 #define LASER_PIN 13
@@ -35,5 +35,5 @@
 // Other constants
 #define SERIAL_BAUD_RATE 9600
 #define STEPPER_TYPE 4
-
+//Test
 #endif
