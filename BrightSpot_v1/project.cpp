e85d1178aa2351785d779b82380f0dfdb96a1bfa
spoea <aspoelst@uwaterloo.ca>
2025-03-21 01:07:18 -0400

    tuning

--- BrightSpot_v1/project.cpp
+++ BrightSpot_v1/project.cpp
@@ -13,8 +13,8 @@ const float kp_y = 800.0;
 const float ki_y = 0.1;
 const float kd_y = 150;
 
-const float kp_c = 50;
-const float ki_c = 0.1;
+const float kp_c = 60;
+const float ki_c = 0.05;
 const float kd_c = 0;
 
 float sumErr_x = 0.0;
